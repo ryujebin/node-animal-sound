41db0e0a14659760481047c8f36b4d0532f016c6
ryujebin <129826948+ryujebin@users.noreply.github.com>
2023-05-02 14:56:20 +0900

    first commit
    동물소리 api(처음)

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/sound/:name', (req, res) => {
+  const { name } = req.params
+
+  if(name == "dog"){
+    res.send({'sound' : '멍멍'})
+  }else if(name == "cat"){
+    res.send({'sound' : '야옹'})
+  }else if(name == "pig"){
+    res.send({'sound' : '꿀'})
+  }else{
+    res.send({'sound' : '알수없음'})
+  }
+
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
